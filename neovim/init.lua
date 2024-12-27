7e1952f7d2c7bf0611c45228915450f79c233cba
tinpott <tinpott@pm.me>
2024-12-26 22:24:36 -0500

    update neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,4 +1,27 @@
-require('plugins')
+--
+-- boostrap lazy.nvim
+--
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+--
+-- neovim options
+--
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -11,31 +34,39 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
 
-vim.opt.mouse = 'n'
+vim.opt.mouse = "n"
 
 vim.opt.termguicolors = true
 
-local packer = require('packer')
---packer.sync()
 
-local colorizer = require('colorizer')
-colorizer.setup()
---colorizer.setup{
---	filetypes = { '*' },
---	user_default_options = {
---		rgb_fn = true
---	},
---	buftypes = {}
---}
+--
+-- set up lazy.nvim
+--
 
-local toggleterm = require('toggleterm')
-toggleterm.setup{
-	size = 20,
-	open_mapping = [[<c-`>]],
-	shell = vim.o.shell,
-	autoscroll = true,
-	direction = 'horizontal',
+require("lazy").setup({
+	spec = {
+		{
+			"folke/tokyonight.nvim",
+			lazy = false, -- make sure we load this during startup if it is your main colorscheme
+			priority = 1000, -- make sure to load this before all the other start plugins
+			config = function()
+				vim.cmd([[colorscheme tokyonight]])
+			end,
+		},
+		--{
+		--	"akinsho/toggleterm.nvim",
+		--	version = "*",
+		--	opts = {
+		--		size = 20,
+		--		open_mapping = [[<c-`>]],
+		--		shell = vim.o.shell,
+		--		autoscroll = true,
+		--		direction = "horizontal",
+		--		shade_terminals = true,
+		--		shading_factor = "25"
+		--	}
+		--},
+	},
+	checker = { enabled = true }, -- automatically check for updates
+})
 
-	shade_terminals = true,
-	shading_factor = '25'
-}
--- neovim/lua/plugins.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd [[packadd packer.nvim]] -- Only required if you have packer configured as `opt`
-
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim' -- Packer can manage itself
-	use 'norcalli/nvim-colorizer.lua'
-	use 'akinsho/toggleterm.nvim'
-end)
