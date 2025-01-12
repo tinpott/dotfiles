cc68667cb18137870907ac0ed2f0f64aa9c06d7e
tinpott <tinpott@pm.me>
2025-01-12 16:24:56 -0500

    update neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,28 +1,3 @@
---
--- boostrap lazy.nvim
---
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
-
-
---
--- neovim options
---
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -55,28 +30,27 @@ vim.opt.list = true
 
 
 --
--- set up lazy.nvim
+-- plugins with lazy.nvim
 --
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	spec = {
-		{
-			"folke/tokyonight.nvim",
-			lazy = false, -- make sure we load this during startup if it is your main colorscheme
-			priority = 1000, -- make sure to load this before all the other start plugins
-			config = function()
-				vim.cmd([[colorscheme tokyonight]])
-			end,
-		},
-		{
-			"MeanderingProgrammer/render-markdown.nvim",
---			dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
---		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
---		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-			---@module "render-markdown"
-			---@type render.md.UserConfig
-			opts = {},
-		},
 --		{
 --			"akinsho/toggleterm.nvim",
 --			version = "*",
@@ -90,6 +64,34 @@ require("lazy").setup({
 --				shading_factor = "25"
 --			}
 --		},
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+--			dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+--			dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+--			dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+			---@module "render-markdown"
+			---@type render.md.UserConfig
+			opts = {},
+		},
+		{
+			"windwp/nvim-autopairs",
+			event = "InsertEnter",
+			config = true
+			-- use opts = {} for passing setup options
+			-- this is equivalent to setup({}) function
+		},
+
+		-- color scheme <https://www.lazyvim.org/plugins/colorscheme>
+		{
+			"loctvl842/monokai-pro.nvim",
+			priority = 1000,
+			config = function()
+				vim.cmd([[colorscheme monokai-pro]])
+			end,
+			opts = {
+				filter = "spectrum"
+			}
+		},
 	},
 	checker = { enabled = true }, -- automatically check for updates
 })
