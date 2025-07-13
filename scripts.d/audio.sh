446ed80b27aefda89ee1739fecc25d067451e08e
tinpott <77856809+tinpott@users.noreply.github.com>
2025-07-13 16:01:35 -0400

    update scripts

--- scripts.d/audio.sh
+++ scripts.d/audio.sh
@@ -5,30 +5,60 @@ if [[ $# != 2 ]]; then
 	exit 1
 fi
 
-volumeDiff=5
+volume_diff=5
+ping=false # TODO: implement this and make it configurable
 
 channel=$1
 sink="sink"
 src="source"
+if [ $channel = "sink" ]; then
+	channel="@DEFAULT_SINK@"
+elif [ $channel = "source" ]; then
+	channel="@DEFAULT_SOURCE@"
+else
+	>&2 echo "Illegal channel: $channel"
+	exit 1
+fi
 
 action=$2
-inc="inc"
-dec="dec"
-toggle="toggle"
-
-if [[ $channel == $sink && $action == $inc ]]; then
-	wpctl set-volume @DEFAULT_AUDIO_SINK@ $volumeDiff%+
-elif [[ $channel == $sink && $action == $dec ]]; then
-	wpctl set-volume @DEFAULT_AUDIO_SINK@ $volumeDiff%-
-elif [[ $channel == $sink && $action == $toggle ]]; then
-	wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-elif [[ $channel == $src && $action == $inc ]]; then
-	wpctl set-volume @DEFAULT_AUDIO_SOURCE@ $volumeDiff%+
-elif [[ $channel == $src && $action == $dec ]]; then
-	wpctl set-volume @DEFAULT_AUDIO_SOURCE@ $volumeDiff%-
-elif [[ $channel == $src && $action == $toggle ]]; then
-	wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+if [ $action = "inc" ]; then
+	wpctl set-volume $channel $volume_diff%+
+	if [[ ping = true && $channel = "@DEFAULT_SINK@" ]]; then
+		play "$HOME/Music/ping.aiff" 2> /dev/null &
+	fi
+elif [ $action = "dec" ]; then
+	wpctl set-volume $channel $volume_diff%-
+	if [[ ping = true && $channel = "@DEFAULT_SINK@" ]]; then
+		play "$HOME/Music/ping.aiff" 2> /dev/null &
+	fi
+elif [ $action = "toggle" ]; then
+	wpctl set-mute $channel toggle
 else
-	>&2 echo "Unknown arguments: $channel $action"
+	>&2 echo "Illegal action: $action"
 	exit 1
+fi
+
+if [ $channel = "@DEFAULT_SINK@" ]; then
+	icon=""
+else
+	icon=""
+fi
+volume=$(wpctl get-volume $channel) # TODO: $(wpctl get-volume $channel | awk '{printf("%d%%\n", 100 * $2)}')
+msg_tag="myvolume"
+if [[ $action = "inc" || $action = "dec" ]]; then # FIXME: the icons don't work
+	dunstify \
+		-a "changeVolume" \
+		-u low \
+		-t 3000 \
+		-i audio-volume-high \
+		-h string:x-dunst-stack-tag:$msg_tag \
+		-h int:value:"$volume" "${icon} ${volume}"
+else
+	dunstify \
+		-a "changeVolume" \
+		-u low \
+		-t 3000 \
+		-i audio-volume-muted \
+		-h string:x-dunst-stack-tag:$msg_tag \
+		-h int:value:"$volume" "${icon} ${volume}"
 fi
