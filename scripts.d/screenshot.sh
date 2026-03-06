13162f3e52112de19b95574264bc65d2eb9e6e4b
tinpott <77856809+tinpott@users.noreply.github.com>
2026-03-06 17:45:23 -0500

    update scripts

--- scripts.d/screenshot.sh
+++ scripts.d/screenshot.sh
@@ -2,6 +2,6 @@
 
 pids=$(pgrep -f grimshot)
 if [ -z $pids ]; then
-	grimshot --notify copy area
+	grim -g "$(slurp)" - | wl-copy
+	# TODO: Save screenshots by fixing this syntax: wl-paste "$HOME/Pictures/screenshots/screenshot_$(date +%Y-%m-%d_%H-%M-%S)"
 fi
-
